525f53757911272ca2838f3b1ab47c40f5542986
Eric FISCHER <efischer78@yahoo.fr>
2021-08-19 09:55:55 +0200

    ok

--- templates/network_ubuntu.sh
+++ templates/network_ubuntu.sh
@@ -1,9 +1,21 @@
-cat >/etc/netplan/02-netcfg.yaml <<EOF 
+#!/bin/bash
+set -e
+
+rm /etc/netplan/*
+cat >/etc/netplan/01-netcfg.yaml <<EOF 
 network:
-  version: 2
-  ethernets:
-    {{ .pack.ifname }}:
-      dhcp4: true
+    ethernets:
+        eth0:
+            dhcp4: true
+            match:
+                macaddress: {{ .mac1 }}
+            set-name: eth0 
+        eth1:
+            dhcp4: true
+            match:
+                macaddress: {{ .mac2 }}
+            set-name: eth1 
+    version: 2
 EOF
 netplan apply
 systemctl restart systemd-networkd
