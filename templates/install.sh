c2a9435584ad97797ff7b29342f2b60434a5cd6f
Eric FISCHER <efischer78@yahoo.fr>
2021-09-07 19:31:42 +0200

    ok

--- templates/install.sh
+++ templates/install.sh
@@ -1,8 +1,6 @@
 mkdir -p /tmp/vbox;
 mount -o loop /tmp/VBoxGuestAdditions_{{ .pack.hostVersion }}.iso /tmp/vbox;
-sh /tmp/vbox/VBoxLinuxAdditions.run \
-    || echo "VBoxLinuxAdditions.run exited $? and is suppressed." \
-        "createOrGet more read https://www.virtualbox.org/ticket/12479";
+sh /tmp/vbox/VBoxLinuxAdditions.run --nox11
 umount /tmp/vbox;
 rm -rf /tmp/vbox;
 rm /tmp/VBoxGuestAdditions_{{ .pack.hostVersion }}.iso;
