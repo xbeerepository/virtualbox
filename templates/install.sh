cb8918ee1a56d341584a54a09fa73e1d52b91f7e
Eric FISCHER <efischer78@yahoo.fr>
2021-09-18 16:10:22 +0200

    ok

--- templates/install.sh
+++ templates/install.sh
@@ -1,7 +1,7 @@
-mkdir -p /tmp/vbox;
-mount -o loop /tmp/VBoxGuestAdditions_{{ .pack.hostVersion }}.iso /tmp/vbox;
-sh /tmp/vbox/VBoxLinuxAdditions.run --nox11
-umount /tmp/vbox;
-rm -rf /tmp/vbox;
-rm /tmp/VBoxGuestAdditions_{{ .pack.hostVersion }}.iso;
+mkdir -p /mnt/vbox
+mount /dev/sr1 /mnt/vbox
+sh /mnt/vbox/VBoxLinuxAdditions.run --nox11
+umount /dev/sr1
+rm -rf /mnt/vbox
+
 
