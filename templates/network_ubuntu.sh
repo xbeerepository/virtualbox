e9d6f45a820f4dffbfeca4925c2b4969b7c227e8
Eric FISCHER <efischer78@yahoo.fr>
2021-09-29 14:06:42 +0200

    ok

--- templates/network_ubuntu.sh
+++ templates/network_ubuntu.sh
@@ -2,18 +2,20 @@
 set -e
 
 rm /etc/netplan/*
+mac1=$(VBoxControl --nologo guestproperty get mac1 | cut -c 8-)
+mac2=$(VBoxControl --nologo guestproperty get mac2 | cut -c 8-)
 cat >/etc/netplan/01-netcfg.yaml <<EOF 
 network:
     ethernets:
         eth0:
             dhcp4: true
             match:
-                macaddress: {{ .mac1 }}
+                macaddress: ${mac1}
             set-name: eth0 
         eth1:
             dhcp4: true
             match:
-                macaddress: {{ .mac2 }}
+                macaddress: ${mac2}
             set-name: eth1 
     version: 2
 EOF
